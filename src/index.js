acceb9d117ff83409ca28ffb660382ae78d9aaf9
Xavier Villavicencio <60786295+Graclyxz@users.noreply.github.com>
2025-01-27 15:04:11 -0500

    render upp

--- src/index.js
+++ src/index.js
@@ -32,7 +32,11 @@ app.set("port", process.env.PORT || 5000); // se configura el puerto a usar si o
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      "http://localhost:5173",
+      "https://lck-front.onrender.com",
+    ],
   })
 );
 app.use(express.json());
