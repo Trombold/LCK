57fe1aeed762fb0ac68de3430f20ac0e8ca5dcf5
Xavier Gutierrez <xagp_07@hotmail.com>
2025-01-27 12:13:32 -0500

    Update index.js
    process.env.FRONTEND_URL

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.set("port", process.env.PORT || 5000); // se configura el puerto a usar si o
 app.use(
   cors({
     credentials: true,
-    origin: "https://lck-front.onrender.com" || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
   })
 );
 app.use(express.json());
